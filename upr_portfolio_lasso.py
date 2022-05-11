aeae9ea7a17a6b8c88c406798ce362b04baf6bd5
SungChul Hong <chulhongsung@gmail.com>
2022-05-11 20:23:23 +0900

    add upr_portfolio_lasso.py

--- upr_portfolio_lasso.py
+++ upr_portfolio_lasso.py
@@ -3,18 +3,28 @@ import numpy as np
 import pandas as pd
 
 import tensorflow as tf
+
+from cvxopt import matrix, solvers
+
 from utils import *
 
+solvers.options['show_progress']=False
+
 DATA_PATH = os.getcwd() + "/data.npy"
+PARAM_PATH = os.getcwd() + "/param.csv"
+PI_PATH = os.getcwd() + "/pi.csv"
 
 X_total_ = np.load(DATA_PATH)
+param_init = pd.read_csv(PARAM_PATH)
+pi_init = pd.read_csv(PI_PATH)
 
-X_total = np.delete(X_total_, 122, 1)
-
+X_total = np.delete(X_total_, 122, axis=1)
 N = X_total.shape[0]
 p = X_total.shape[1]
 
-df_pi = pd.DataFrame(columns=np.arange(p))
+lambda_1 = 0.01
+
+df_pi = pd.DataFrame(columns=np.arange(162))
 df_pi = df_pi.add_prefix('pi_')
 
 gamma_name_lst = ['gamma']
@@ -25,28 +35,38 @@ name_lst = gamma_name_lst
 
 df_param = pd.DataFrame(columns=name_lst)
 
+D = np.hstack([np.eye(p), -1*np.eye(p)])
+
+P = matrix(D.transpose() @ D)
+
+G = matrix(-1*np.eye(2*p))
+
+h = matrix(np.zeros(2*p))
+
 i = 1
 
 while (240+60*(i-1)) <= N:
     
     x  = tf.constant(X_total[(60*(i-1)):(240+60*(i-1)),:], dtype=tf.float32)
-
-    mu_hat = tf.reduce_mean(x, axis=0)[:, tf.newaxis]
     mu_market = tf.reduce_mean(x)
+    mu_hat = tf.reduce_mean(x, axis=0)[:, tf.newaxis]
     mu_hat_squad = tf.linalg.matmul(mu_hat, mu_hat, transpose_a=True)
     mu_hat_sum = tf.math.reduce_sum(mu_hat)
     tmu = mu_market + 0.005
-    pi = tf.Variable(tf.random.uniform([p], minval=0, maxval=0.1, dtype=tf.dtypes.float32)[:, tf.newaxis])
+   
+    A = matrix(np.vstack((mu_hat.numpy().transpose() @ np.hstack([np.eye(p), -1*np.eye(p)]), np.hstack([np.ones((1, p), np.float32), -1*np.ones((1, p), np.float32)]), np.ones((1, 2*p), np.float32))))
 
-    beta = tf.Variable(tf.random.uniform([17], minval=0.0, maxval=0.1, dtype=tf.dtypes.float32)[tf.newaxis,:])
+    pi = tf.Variable(np.array([pi_init.iloc[i-1]], np.float32).transpose())
+    
+    beta = tf.Variable(np.array(param_init.iloc[i-1, 1:], dtype=np.float32)[tf.newaxis,:])
 
     delta = tf.constant(np.append(np.append([0.0, 0.025, 0.025, 0.025, 0.025], np.repeat(1, 8)/10), [0.025, 0.025, 0.025, 0.025]), dtype=tf.dtypes.float32)
-
-    gamma = tf.Variable(tf.constant(np.array([-0.1]), dtype=tf.dtypes.float32))
-
+    
+    gamma = tf.Variable(tf.constant(np.array(param_init.iloc[i-1, 0]), dtype=tf.dtypes.float32))
+    
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-    for j in range(1000):
+    for j in range(70):
         with tf.GradientTape() as tape:
             quantile_dl = tf.squeeze(tf.map_fn(lambda x: linear_spline(x, gamma, beta, delta), tf.math.cumsum(delta)))
             
@@ -54,27 +74,31 @@ while (240+60*(i-1)) <= N:
         
             upr_  = tf.map_fn(lambda x: upr(x, gamma, beta, delta, quantile_dl), portfolio)
                         
-            loss = tf.reduce_mean(upr_)
+            loss = tf.reduce_mean(upr_) + lambda_1 * tf.norm(pi, ord=1)
 
             if j == 0:
                 print(loss)
-
-            if (j+1) % 100 == 0:
+                 
+            if (j+1) % 10 == 0:
                 print(loss)
-            
+
         grads = tape.gradient(loss, [pi, gamma, beta])
         optimizer.apply_gradients(zip(grads, [pi, gamma, beta]))
 
+        q = matrix((-1)*(pi.numpy().transpose() @ D).transpose())
+        b = matrix(np.array([tmu.numpy().item(), 1, np.round(tf.reduce_sum(tf.abs(pi)).numpy().item(), 4)]))
+        
+        sol = solvers.qp(P=P, q=q, G=G, h=h, A=A, b=b)
+        pi_tmp = np.array(sol['x'])
+        pi = tf.Variable(np.hstack([np.eye(p), -1*np.eye(p)]) @ pi_tmp, dtype=tf.float32)
+        
         beta = tf.Variable(tf.clip_by_value(beta, 0., float("inf")), dtype=tf.dtypes.float32)
-        eta_1 = (mu_hat_squad*(tf.math.reduce_sum(pi) - 1) - mu_hat_sum * (tf.linalg.matmul(mu_hat, pi, transpose_a=True) - tmu))/(p*mu_hat_squad - mu_hat_sum**2)
-        eta_2 = (mu_hat_sum * tf.math.reduce_sum(pi) - mu_hat_sum - p * tf.linalg.matmul(mu_hat, pi, transpose_a=True) + p * tmu)/(mu_hat_sum**2 - p*mu_hat_squad)
-        pi =  tf.Variable(pi - eta_1 - eta_2 * mu_hat)
-
+    
     tmp_pi = pd.DataFrame(np.squeeze(pi.numpy()))
     tmp_pi = tmp_pi.transpose().add_prefix('pi_')
     df_pi = pd.concat([df_pi,tmp_pi],axis=0) 
     
-    tmp_param = pd.DataFrame(np.concatenate([gamma.numpy(), np.squeeze(beta.numpy())]))
+    tmp_param = pd.DataFrame(np.concatenate([np.array(gamma.numpy())[np.newaxis,...], np.squeeze(beta.numpy())]))
     tmp_param = tmp_param.transpose()
     tmp_param.columns = name_lst
     df_param = pd.concat([df_param, tmp_param], axis=0)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 def linear_spline(alpha, gamma, beta, delta):
     mask = alpha - tf.math.cumsum(delta) >= 0
@@ -27,3 +29,71 @@ def upr(z, gamma, beta, delta, quantile_dl):
     upr = (1+tf.math.log(tilde_a))*(z-gamma) + tf.math.reduce_sum(bl*(((-1/2) * ((1-dl)**2)) + 1 - tf.maximum(tilde_a, dl) + dl * tf.maximum(tf.math.log(tilde_a), tf.math.log(dl))))
     
     return upr
+
+def empirical_upr(sort_return, num_obs=60, num_knots=10):
+    """
+        Calculate empirical UPR 
+
+    
+    Args:
+        sort_return ([type]): [description]
+        num_knots ([type]): [description]
+    """
+
+    w = np.log(np.arange(1,num_obs+1) / num_obs)
+    
+    w_star = w / np.sum(w)
+    
+    sort_return_ = sort_return.reshape((-1, int(len(sort_return)/num_knots)))
+    return_sum_quantile = np.cumsum(np.sum(sort_return_, axis=-1))
+    
+    alpha_risk = return_sum_quantile/(len(sort_return)/num_knots * np.arange(1, 11))
+    
+    upr = np.sum(w_star * sort_return)
+    
+    return alpha_risk, upr
+
+def quantile_matching_figure(data, param, pi, start, end):
+    """
+    Args:
+    
+    Returns:
+        plot
+    """
+    # define subplot grid
+    _, axs = plt.subplots(nrows=4, ncols=4, figsize=(28, 28))
+    plt.subplots_adjust(hspace=0.2)
+    # loop through tickers and axes
+    for num_window, ax in zip(range(start, end+1), axs.ravel()):
+        gamma = np.array([param.iloc[num_window-1,0]], np.float32)
+        beta = np.array([param.iloc[num_window-1,1:]], np.float32)
+        
+        X = data[(60*(num_window-1)):(240+60*(num_window-1)),:]     
+        out_X = data[(240+60*(num_window-1)):(240+60*num_window),:] 
+
+        portfolio = X @ np.array([pi.iloc[num_window-1]], np.float32).transpose()
+        out_portfolio = out_X @ np.array([pi.iloc[num_window-1]], np.float32).transpose()
+        
+        empirical_quantile = np.quantile(portfolio, np.linspace(0.0, 1, 11, dtype=np.float32))
+        out_empirical_quantile = np.quantile(out_portfolio, np.linspace(0.0, 1, 11, dtype=np.float32))
+
+        quantile_ = np.linspace(0.0, 1, 100, dtype=np.float32)
+        estimated_return = tf.squeeze(tf.map_fn(lambda x: linear_spline(x, gamma, beta, delta), tf.constant(quantile_)))
+        # filter df for ticker and plot on specified axes
+        if (num_window % 4) == 1:
+            ax.plot(np.linspace(0.0, 1, 100, dtype=np.float32), estimated_return.numpy(), 'g-', lw=3, label='Estimated Quantiles')
+            ax.plot(np.linspace(0.0, 1, 11, dtype=np.float32), empirical_quantile, 'b--', lw=3, label='Empirical Quantiles')
+            ax.plot(np.linspace(0.0, 1, 11, dtype=np.float32), out_empirical_quantile, 'm:', lw=3, label='Out-of-sample Quantiles')
+        else: 
+            ax.plot(np.linspace(0.0, 1, 100, dtype=np.float32), estimated_return.numpy(), 'g-', lw=3)
+            ax.plot(np.linspace(0.0, 1, 11, dtype=np.float32), empirical_quantile, 'b--', lw=3)
+            ax.plot(np.linspace(0.0, 1, 11, dtype=np.float32), out_empirical_quantile, 'm:', lw=3)
+        
+        ax.set_xlabel("Quantiles", fontsize=20)
+        if (num_window % 4) == 1:
+            ax.set_ylabel("Portfolio Return", fontsize=20)
+        if (num_window == 1 )| (num_window  == 17):
+            ax.legend(fontsize=20)
+
+        # chart formattin
+    plt.show()
